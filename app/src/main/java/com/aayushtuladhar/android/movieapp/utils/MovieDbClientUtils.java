e2570c998003bc84394da2014935f895d2f5c8ef
Aayush Tuladhar <aayush.tuladhar@gmail.com>
2018-05-27 15:31:57 -0500

    Code Review Comments and Suggestions Fix

--- app/src/main/java/com/aayushtuladhar/android/movieapp/utils/MovieDbClientUtils.java
+++ app/src/main/java/com/aayushtuladhar/android/movieapp/utils/MovieDbClientUtils.java
@@ -1,5 +1,6 @@
 package com.aayushtuladhar.android.movieapp.utils;
 
+import com.aayushtuladhar.android.movieapp.BuildConfig;
 import com.aayushtuladhar.android.movieapp.data.Select;
 import com.aayushtuladhar.android.movieapp.data.remote.MoviedbService;
 import com.aayushtuladhar.android.movieapp.data.remote.RetrofitClient;
@@ -11,7 +12,7 @@ public class MovieDbClientUtils {
 
     public static final String BASE_URL = "http://api.themoviedb.org";
     public static final String POSTER_BASEURL = "http://image.tmdb.org/t/p/w185/";
-    private static final String API_KEY = "ae7befa83ef7286b1d688bde98b38ee3";
+    private static final String API_KEY = BuildConfig.MOVIEDB_API_KEY;
 
     public static Call<MovieResponse> getPopularMovies(Select select){
         MoviedbService moviedbService = RetrofitClient.getClient(BASE_URL).create(MoviedbService.class);
