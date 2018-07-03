dcc855e786915c82d689198a726a962d6952dd99
Aayush Tuladhar <aayush.tuladhar@gmail.com>
2018-07-02 23:51:09 -0500

    Adding Preference - Part 2

--- app/src/main/java/com/aayushtuladhar/android/movieapp/MainActivity.java
+++ app/src/main/java/com/aayushtuladhar/android/movieapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.aayushtuladhar.android.movieapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +27,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     private static final String TAG = MainActivity.class.getCanonicalName();
 
@@ -33,7 +35,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getMoviesfromApi(this, Select.MOST_POPULAR);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        // Register onSharedPreferenceChangeListener to Activity
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        Select sort = getSortFromPreferences();
+        getMoviesfromApi(this, sort);
+    }
+
+    private Select getSortFromPreferences(){
+        SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(this);
+
+        final String settingSortKey = getString(R.string.settings_sort_by_key);
+
+        String sortBy = sharedPreference.getString(settingSortKey, "");
+        switch (sortBy){
+            case "Most Popular":
+                Log.i(TAG, "Most Popular Selected");
+                return Select.MOST_POPULAR;
+            case "Highest Rated":
+                Log.i(TAG, "Top Rated Movies Selected");
+                return Select.TOP_RATED;
+        }
+        Log.e(TAG, "For Unknown Preferences; return MOST_POPULAR");
+        return Select.MOST_POPULAR;
     }
 
     private void getMoviesfromApi(final Context context, Select select){
@@ -97,4 +122,19 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.settings_sort_by_key))){
+            Select sort = getSortFromPreferences();
+            //Refresh List
+            getMoviesfromApi(this, sort);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences sharedPreference = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreference.unregisterOnSharedPreferenceChangeListener(this);
+    }
 }
--- app/src/main/java/com/aayushtuladhar/android/movieapp/data/Select.java
+++ app/src/main/java/com/aayushtuladhar/android/movieapp/data/Select.java
@@ -1,3 +1,5 @@
 package com.aayushtuladhar.android.movieapp.data;
 
-public enum Select {MOST_POPULAR, TOP_RATED}
+public enum Select {
+    MOST_POPULAR, TOP_RATED;
+}
